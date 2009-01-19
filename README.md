681fae5bf75bf1742833ecd93681282a4c646455
Leslie Michael Orchard <l.m.orchard@pobox.com>
2009-01-19 12:52:14 -0500

    initial commit;

---
+++ README.md
@@ -0,0 +1,7 @@
+= Twitter friend faves OPML =
+
+This is a quick & dirty PHP page that, given a screen name, will fetch the
+friends list for that screen name and generate an OPML reading list of all the
+friends' favorites' Atom feeds.  This, then, can be imported into Google Reader
+or other feed aggregators.
+
---
+++ index.php
@@ -0,0 +1,90 @@
+<?php
+    $user_name = 'lmorchard';
+    $password = 'not my password';
+    $page_title = "twitter friends' faves as OPML";
+
+    $base_url = $_SERVER['REQUEST_URI'];
+    function e($str) {
+        echo htmlspecialchars($str);
+    }
+?>
+<?php if (empty($_GET['screenname'])): ?>
+<html>
+    <head>
+        <title><?php e($page_title) ?></title>
+        <style type="text/css">
+            form { }
+            form fieldset { }
+            form fieldset legend { }
+            form fieldset ul { list-style-type: none }
+            form fieldset ul li { padding-bottom: 0.5em }
+            form fieldset ul li label, form fieldset ul li span 
+                { float: left; text-align: right; width: 12ex; padding-right: 0.25em; }
+            form fieldset ul li label:after { content: ":"; }
+            form fieldset ul li input { }
+        </style>
+    </head>
+    <body>
+        <h1><?php e($page_title) ?></h1>
+        <form method="GET">
+            <fieldset>
+                <legend>user details</legend>
+                <ul>
+                    <li>
+                        <label>screen name</label>
+                        <input name="screenname" type="text" />
+                    </li>
+                    <li>
+                        <span>&nbsp;</span>
+                        <input type="submit" />
+                    </li>
+                </ul>
+            </fieldset>
+        </form>
+    </body>
+</html>
+<?php else: ?>
+<?php
+    $screenname = $_GET['screenname'];
+    $ch = curl_init('http://twitter.com/statuses/friends/'.$screenname.'.json');
+    curl_setopt_array($ch, array(
+        CURLOPT_USERAGENT      => 'twitter-friend-faves-opml/0.1',
+        CURLOPT_FAILONERROR    => true,
+        CURLOPT_RETURNTRANSFER => true,
+        // CURLOPT_USERPWD        => $user_name . ':' . $password
+    ));
+    $resp = curl_exec($ch);
+    $info = curl_getinfo($ch);
+    curl_close($ch);
+    $friends = json_decode($resp);
+
+    // Start off the OPML document with an XmlWriter.
+    require_once 'lib/XmlWriter.php';
+    $x = new Memex_XmlWriter(array(
+        'parents' => array( 'opml', 'head', 'body', 'outline')
+    ));
+    $title = "$screenname's friends' twitter favorites";
+    $x->opml()
+        ->head()
+            ->title($title)
+        ->pop()
+        ->body()
+        ->outline(array('title'=>$title));
+
+    // Add each of the posts as OPML outline elements, with details 
+    // encoded in attributes.
+    foreach ($friends as $friend) {
+        $x->emptyelement('outline', array(
+            'type'    => 'rss',
+            'version' => 'atom',
+            'title'   => $friend->name . "'s twitter favorites",
+            'xmlUrl'  => 'http://twitter.com/favorites/'.$friend->screen_name.'.atom'
+        ));
+    }
+
+    $x->pop();
+
+    header('Content-Type: text/xml+opml');
+    echo $x->getXML();
+?>
+<?php endif ?>
